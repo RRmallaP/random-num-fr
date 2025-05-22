23665395e874915f43e0ee0f8b4dcab2a33712c6
Rabi Malla <rabi.malla@outlook.com>
2025-05-22 23:42:58 +0545

    added speech functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,6 @@ function App() {
 
   return (
     <>
-      <h3>Include Vocab item component here</h3>
-      <VocabularyItem vocabItem={{fr_name: 'pomme', en_name: 'apple', pronunciation: 'po mm', sentence: 'Il y a une pomme sur la table.'}}>
-      </VocabularyItem>
-
       <RandomNumberGame></RandomNumberGame>
     </>
   )
--- src/components/random-number/random-number.jsx
+++ src/components/random-number/random-number.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./random-number.css";
 
+const voices = speechSynthesis
+      .getVoices()
+      .filter(voice => voice.lang === "fr-FR");
+
+function utterNumber(number) {
+  const synth = new SpeechSynthesisUtterance(number);
+  synth.lang = "fr-FR";
+  synth.rate = 0.75;
+  synth.voice = voices[9];
+
+  speechSynthesis.speak(synth);
+}
+
 function getRandomNumber() {
-  return Math.floor((Math.random() * 100) + 1);
+  const randNum = Math.floor((Math.random() * 100) + 1);
+  utterNumber(randNum);
+  return randNum;
 }
 
 function RandomNumberGame() {
@@ -11,9 +26,12 @@ function RandomNumberGame() {
   let inputNumber;
   let [checkStatus, setCheckStatus] = useState(false);
   let [isCorrect, setIsCorrect] = useState(false);
+
+  const numberInputRef = useRef(null);
   console.log(randomNumber);
 
-  function checkInput() {
+  function checkInput(event) {
+    event.preventDefault();
     setIsCorrect(+inputNumber === randomNumber);
     console.log(inputNumber);
     setCheckStatus(true);
@@ -26,48 +44,53 @@ function RandomNumberGame() {
 
   function handleNext() {
     setRandomNumber(getRandomNumber());
-    inputNumber = null;
+    numberInputRef.current.value = '';
+    numberInputRef.current.focus();
+    setCheckStatus(false);
   }
 
   return (
     <div className="flex items-center justify-center bg-gray-700 min-h-dvh">
       <div className="p-6 bg-white rounded-lg border-3 min-w-2xl">
-        <div className="input-wrapper pb-4 mb-5">
-          <label htmlFor="number-input" className="pe-4 text-xl font-semibold">
-            Enter the number you heard:
-          </label>
-          <input 
-            id="number-input"
-            className="px-3 outline outline-1 rounded-md min-w-40"
-            type="number" 
-            onChange={(event) => handleInput(event)} 
-            max={100} 
-            min={1} />
-        </div>
-        <div className=" flex gap-10 pb-4 mb-5">
-          <button className="bg-blue-500 px-8 py-4 rounded-sm" 
-            type="button" 
-            onClick={checkInput} >
-            Check
-          </button>
-          <button className="bg-yellow-500 px-8 py-4 rounded-sm" 
-          type="button"
-          onClick={handleNext}>
-            Next
-          </button>
-        </div>
+        <form onSubmit={(event) => checkInput(event)}>
+          <div className="input-wrapper pb-4 mb-5">
+            <label htmlFor="number-input" className="pe-4 text-xl font-semibold">
+              Enter the number you heard:
+            </label>
+            <input 
+              ref={numberInputRef}
+              id="number-input"
+              className="px-3 outline outline-1 rounded-md min-w-40"
+              type="number" 
+              onChange={(event) => handleInput(event)} 
+              max={100} 
+              min={1} />
+          </div>
+          <div className=" flex gap-10 pb-4 mb-5">
+            <button className="bg-blue-500 px-8 py-4 rounded-sm" 
+              type="submit" disabled={checkStatus}>
+              Check
+            </button>
+            <button className="bg-yellow-500 px-8 py-4 rounded-sm" 
+            type="button"
+            disabled={!checkStatus}
+            onClick={handleNext}>
+              Next
+            </button>
+          </div>
 
-        { checkStatus && isCorrect &&
-          <h3 className="pb-4 mb-5">
-            Bravo, its correct. Click Next button above to try another one.
-          </h3>
-        }
+          { checkStatus && isCorrect &&
+            <h3 className="pb-4 mb-5">
+              Bravo, its correct. Click Next button above to try another one.
+            </h3>
+          }
 
-        { checkStatus && !isCorrect &&
-          <h3 className="pb-4 mb-5">
-            Wrong one, try again or go to next one.
-          </h3>
-        }
+          { checkStatus && !isCorrect &&
+            <h3 className="pb-4 mb-5">
+              Wrong one, try again or go to next one.
+            </h3>
+          }
+        </form>
       </div>
     </div>
   );
